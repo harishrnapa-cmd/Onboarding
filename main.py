1a79123971b05a82b6948d682055a4da9079c90c
harishrnapa-cmd <harishrnapa@gmail.com>
2026-02-13 12:14:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,40 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
+import sys
+
+def removeDeadColumns (df):
+  droppedColumns= []
+
+  for col in df.columns:
+    if "Analog Input" in col and df[col].nuniuque()==1:
+      droppedColumns.append(col)
+
+  df= df.drop(columns=droppedColumns)
+  
+  print(f"Removed dead sensors: {droppedColumns}")
+  return df
+
+def graphPlot(df)
+  if "RPM" in df.columns:
+    fig_rpm= px.line(df, y="RPM", title="RPM Over Time")
+    fig_rpm.show()
+
+  if "TPS" in df.columns:
+    fig_tps= px.line(df, y="TPS", title="TPS Over Time")
+    fig_tps.show()
+
+def main():
+  if len(sys.argv) !=2:
+    print("Invalid")
+    return
+
+  file_path= sys.argv[1]
+
+df= pd.read_csv(file_path)
+
+df = removeDeadColumns(df)
+
+graphPlot(df)
+
+if _name_ == "_main_":
+  main()
